6fb59619b0bd0527be6216e2714bb894acf86cae
Kyle McLuckie <kyle.mcluckie@gmail.com>
2025-08-03 20:32:12 +0800

    Reinstall

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/test-utils'],
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-07-16'
+})
