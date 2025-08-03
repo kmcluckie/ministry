d59dfbad6f9a3f124b24c9e34190a021fd0129d9
Kyle McLuckie <kyle.mcluckie@gmail.com>
2025-08-03 21:25:55 +0800

    Fix supabase issue with tailwind styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/test-utils'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/test-utils', '@nuxtjs/supabase'],
 
   css: ['~/assets/css/main.css'],
 
-  compatibilityDate: '2025-07-16'
+  ssr: false,
+
+  compatibilityDate: '2025-07-16',
+
+  supabase: {}
 })
